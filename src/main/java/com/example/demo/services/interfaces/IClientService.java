bef507609d625795f8d768da9bde265458886aeb
Luis Eduardo <213355@ids.upchiapas.edu.mx>
2022-10-19 22:01:19 -0500

    tirdh commit

--- src/main/java/com/example/demo/services/interfaces/IClientService.java
+++ src/main/java/com/example/demo/services/interfaces/IClientService.java
@@ -1,7 +1,9 @@
 package com.example.demo.services.interfaces;
 
 import com.example.demo.controllers.dtos.request.CreateClientRequest;
+import com.example.demo.controllers.dtos.request.CreateDestinationRequest;
 import com.example.demo.controllers.dtos.responses.CreateClientResponse;
+import com.example.demo.controllers.dtos.responses.CreateDestinationResponse;
 
 import java.util.List;
 
